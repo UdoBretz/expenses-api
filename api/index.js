97c2b58fdab5ac6fd5c393e65a86bac32a8d69e6
Udo Bretz <udo.bretz@auth0.com>
2021-06-22 00:52:09 +0200

    Add auth midlleware

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const cors = require("cors");
-const { createServer } = require("http");
-// 👉 Replace this with express-oauth2-bearer require 👈
+const express = require('express');
+const cors = require('cors');
+const { createServer } = require('http');
+const { auth } = require('express-oauth2-bearer');
 const {
   checkUrl,
   APP_URL, // Public URL for this app
   ISSUER_BASE_URL, // Auth0 Tenant Url
   ALLOWED_AUDIENCES, // Auth0 API Audience List
   PORT,
-} = require("./env-config");
+} = require('./env-config');
 
 const app = express();
 
@@ -20,12 +20,12 @@ app.use(cors());
 const expenses = [
   {
     date: new Date(),
-    description: "Pizza for a Coding Dojo session.",
+    description: 'Pizza for a Coding Dojo session.',
     value: 102,
   },
   {
     date: new Date(),
-    description: "Coffee for a Coding Dojo session.",
+    description: 'Coffee for a Coding Dojo session.',
     value: 42,
   },
 ];
@@ -37,19 +37,21 @@ const expenses = [
  * from preventing the user to make
  * requests via the browsers console.
  ****************************/
-app.get("/", (req, res) => {
-  res.status(200).end("OK");
+app.get('/', (req, res) => {
+  res.status(200).end('OK');
 });
 /****************************/
 
-app.get("/total", (req, res) => {
+app.get('/total', (req, res) => {
   const total = expenses.reduce((accum, expense) => accum + expense.value, 0);
   res.send({ total, count: expenses.length });
 });
 
-// 👉 Replace this with auth middleware 👈
+// 👆 public routes above 👆
+app.use(auth());
+// 👇 private routes below 👇
 
-app.get("/reports", (req, res) => {
+app.get('/reports', (req, res) => {
   res.send(expenses);
 });
 
